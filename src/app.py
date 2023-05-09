086166bef3341fd7a353e9a58576629f4cbc73b0
NikoDemo96 <chnicolas.aranguiz@gmail.com>
2023-05-09 05:21:23 +0000

    anotaciones de la clase

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character
 #from models import Person
 
 app = Flask(__name__)
@@ -36,6 +36,8 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+#Trabajar a partir de aqui para la tarea
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -44,6 +46,117 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+# Todos los personajes
+@app.route("/character", methods=["GET"])
+def get_all_characters():
+    character_list = Character.query.all()
+    serialized_characters = [character.serialize() for character in character_list]
+    return jsonify({"characters": serialized_characters})
+
+@app.route("/character", methods=["POST"])
+def add_character():
+    body = request.json
+    body_name = body.get("name", None)
+    body_eye_color = body.get("eye_color", None)
+    body_hair_color = body.get("hair_color", None)
+    body_gender = body.get("gender", None)
+
+    #Validar para verificar que todos los campos eestan siendo enviados. Si falta un campo, manda error.
+    if body_name is None or body_eye_color is None or body_hair_color is None or body_gender is None:
+        return {"error": f"todos los campos son requeridos"}, 400
+
+    #Validacion para ver si el personaje ya existe
+    character_exists = Character.query.filter_by(name=body_name).first()
+    if character_exists:
+        return {"Error": f"Ya existe un personaje con el nombre: {body_name}"}
+    
+    #Creando una nueva instancia del modelo para agregar un personaje nuevo
+    #Una instancia en la clase character es Han Solo, Luke, etc. Una nueva instancia es un nuevo elemento dentro de la clase.
+    new_character = Character(name=body_name, eye_color=body_eye_color, hair_color=body_hair_color, gender=body_gender)
+
+    db.session.add(new_character)
+
+    #Try and catch para evitar errores en la base de datos
+    #Si en parte de mi Try hay un commit, en mi Exception tiene que haber un rollback.
+    #En todos los endpoints que impliquen crear: Meter el commit dentro del try y el rollback dentro del Exception.
+    try: 
+        db.session.commit()
+        return jsonify({"data": f"Personaje {body_name} creado con exito"}), 201
+    
+    #Si hay un error, atrapo el error y retorno el error.
+    except Exception as error:
+        db.session.rollback()
+        return {"error": error}, 500
+
+
+#Hacemos una ruta dinamica para buscar por ID  aun personaje en especifico
+@app.route("/character/<int:id>", methods=["GET"])
+def get_character_by_id(id):
+    character_exists = Character.query.filter_by(id=id).first()
+    if not character_exists:
+        return {"error": "No existe un personaje con ese id"}, 404
+    
+    return jsonify ({"personaje": character_exists.serialize()})
+
+#Todos los planetas
+@app.route("/planets", methods=["GET"])
+def get_planets():
+    # Model.query.all() --> trae todos los elementos
+    planets = Planet.query.all() # Trae todos
+    # nueva_lista = [item.serialize() for item in list]
+    print(planets)
+    serialized_planets = [planet.serialize() for planet in planets] # Comprension de listas
+    print(serialized_planets)
+    return jsonify({"data": serialized_planets}) # Ahora me trae una lista con 2 planetas
+
+#Trae UN planeta
+@app.route("/planet", methods=['GET'])
+def get_planet():
+    test_id = 1
+    #Model.query.filter_by(campovalor)
+    planet = Planet.query.filter_by(id=test_id).one_or_none()
+    print("serializado")
+    print(planet.serialize())
+    print("sin serializar")
+    print(planet)
+    return jsonify({"data": planet.serialize()})
+
+@app.route("/planet", methods=["POST"])
+def add_planet():
+    # Que campos necesito para crear un planeta
+    # --> Body = {name, etc}
+    body = request.json #Busco el json del body
+    #Validaciones:
+    body_name = body.get("name", None) # Extraigo el name del body con el metodo get. El
+    #el metodo get va a buscar una propiedad que se llame "name" y sino existe, le pone "none". Si no existe la variable, vale none
+    #Le colocamos el valor por defecto "none" para poder atajar mi error con un if. Entonces estoy agregando la validacion del campo.
+    #Estoy agregando que como minimo el "name" exista.
+    #le colocamos un valor por defecto para hacer un if name 
+    if body_name is None:
+        return {"error": "Todos los campos requeridos"}, 400
+
+    #Otra validacion para ver si ya existe un planeta con ese nombre
+    planet_exists = Planet.query.filter_by(name=body_name).first()
+    if planet_exists:
+        return{"Error": f"Ya existe un planeta con el nombre: {body_name}"}
+
+    # Creando una nueva instancia del modelo.
+    new_planet = Planet(name=body_name) 
+    
+    db.session.add(new_planet) # Le estoy agregando a mi sesion de base de datos un planeta nuevo. Cuando uno corre el servidor y se va a hacer una consulta a la base de datos, el crea algo que se llama sesion. Y uno accede a esa sesion atra vez de db.session.
+    
+    db.session.commit() # Con este comando, guardamos el cambio.
+
+    print(body)
+    #Luego de usar session.add y session.commit, uno suele regresar un jsonify
+    #Status 201 --> Es para cuando uno crea algo en la base de datos
+    return jsonify({"data": f"Planeta {body_name} creado con exito"}), 201
+
+
+
+#Termina de trabajar tarea aqui
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,63 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+#Modelos para starwars API
+#Despues de modificar/agregar un modelo a la BDD, correr pipenv run migrate y pipenv run upgrade.
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    eye_color = db.Column(db.String(30), nullable=True, default="N/A")
+    hair_color = db.Column(db.String(30), nullable=True, default="N/A")
+    gender = db.Column(db.String(30), nullable=True, default="N/A")
+    birth_year = db.Column(db.String(30), nullable=True)
+    height = db.Column(db.Integer, nullable=True)
+    mass = db.Column(db.Integer, nullable=True)
+    homeworld = db.Column(db.String(30), nullable=True)
+
+
+    #Estos son metodos de visualizacion. Cuando yo pido algo a la base de datos, puedo especificar que me puede traer.
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.name,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "height": self.height,
+            "mass": self.mass,
+            "homeworld": self.homeworld,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    population = db.Column(db.Integer, nullable=True)
+    climate = db.Column(db.String(30), nullable=True)
+    terrain = db.Column(db.String(30), nullable=True)
+    diameter = db.Column(db.Integer, nullable=True)
+    rotation_period = db.Column(db.Integer, nullable=True)
+    orbital_period = db.Column(db.Integer, nullable=True)
+    gravity = db.Column(db.String(30), nullable=True)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name #no estoy seguro de si aqui va name o que
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population, 
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+        }
+
+
